6e3d6a9d4e2cb81f6d477debd5079c47332fd4af
ahmed491guy <ahmedrasheedh491@gmail.com>
2025-12-26 12:08:09 +0500

    Update scrit.js

---
+++ scrit.js
@@ -0,0 +1,13 @@
+const weatherIcon = document.querySelector(".weather-icon");
+
+if(data.weather[0].main == "Clouds"){
+    weatherIcon.src = "images/clouds.png";
+} else if(data.weather[0].main == "Clear"){
+    weatherIcon.src = "images/clear.png";
+} else if(data.weather[0].main == "Rain"){
+    weatherIcon.src = "images/rain.png";
+} else if(data.weather[0].main == "Drizzle"){
+    weatherIcon.src = "images/drizzle.png";
+} else if(data.weather[0].main == "Mist"){
+    weatherIcon.src = "images/mist.png";
+}
